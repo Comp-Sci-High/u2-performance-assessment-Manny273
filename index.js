6cc9d6da1a545f4eae441e533b821744df4e6327
Manny273 <134006013+Manny273@users.noreply.github.com>
2024-11-13 18:05:37 +0000

    end my suffering

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+const prompt = require('prompt-sync')()
 
+async function aiConvo(bodyData){
+    const options = {
+        method : "POST",
+      headers: {
+        Authorization: "Bearer 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" ,
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(bodyData)
+    };
+
+ 
+    let response = await fetch("https://api.openai.com/v1/chat/completions", options)
+    let data = await response.json()
+    console.log(data.choices[0].message.content)
+}
+let aiConvoBody = {
+    model: "gpt-4o-mini",
+  messages: [
+    {
+      "role": "system",
+      "content":"You are a helpful assistant."
+    },
+    {
+        "role": "user",
+        "content":" "
+    },
+  ]
+}
+let userInput = "";
+let running = true;
+console.log("I am your Chat GPT generated assistant")
+while(running){
+    console.log("What is the question you would like to ask?");
+    userInput = prompt("");
+    if(userInput.includes("no")){
+        break
+    }
+    aiConvoBody.messages[1].content = userInput
+    aiConvo(aiConvoBody)
+    
+}
